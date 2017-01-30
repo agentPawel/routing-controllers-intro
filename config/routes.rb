ea953193ffa2ae3f74cb67aae282827c98961ae9
Pawel Parafianowicz <pawelp@outlook.com>
2017-01-30 14:15:22 -0500

    Create about and contest route commands

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome' => 'pages#welcome'
+
   get '/' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
+
+  get '/contest' => 'pages#contest'
+
 end
