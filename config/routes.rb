42779c7a201e2ef20126454979bc84ceb64822b0
Pawel Parafianowicz <pawelp@outlook.com>
2017-01-30 15:35:20 -0500

    Rake routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#welcome'
+
   get '/welcome' => 'pages#welcome'
 
-  get '/' => 'pages#welcome'
+  get '/about' => 'pages#about', as: 'about'
 
-  get '/about' => 'pages#about'
+  get '/contest' => 'pages#contest', as: 'contest'
 
-  get '/contest' => 'pages#contest'
+  get '/kitten/:size/:size' => 'pages#kitten', as: 'kitten'
 
-  get '/kitten/:size/:size' => 'pages#kitten'
+  get '/kittens/:size/:size' => 'pages#kittens', as: 'kittens'
 
-  get '/kittens/:size/:size' => 'pages#kittens'
-
-  get '/secrets/:magic_word' => 'pages#secrets'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 
 end
